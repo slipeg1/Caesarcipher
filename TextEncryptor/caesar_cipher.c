1c5715a39c81d1bce425548dae540d0d35cac8f4
slipeg1 <slipeg2@hotmail.fr>
2020-01-05 12:12:55 -0500

    Mostly refactored for obvious reason.

--- TextEncryptor/caesar_cipher.c
+++ TextEncryptor/caesar_cipher.c
@@ -3,11 +3,15 @@
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
+#define Jump 2
 
-void Encrypte(char* word){
-	const char* Alphabet = "abcdefghijklmnopqrstuvwxyzabc";
+
+// 1 or 0. If 0 then it will encrypt.
+void ED(char* word, int Decrypte){
+	const char* Alphabet = "xyzabcdefghijklmnopqrstuvwxyzabc";
 	int abc_size = strlen(Alphabet);
 	int word_size = strlen(word);
+	Jump++;
 	char* rtword = (char *)malloc(word_size * sizeof(char));
 
 	for(int i = 0; i<=word_size-1; i++)
@@ -15,10 +19,15 @@ void Encrypte(char* word){
 		for(int e = 0; e<=abc_size; e++)
 		{
 			if(word[i] == Alphabet[e]) {
-				rtword[i] = Alphabet[e+2];
+				if(Decrypte == 0) {
+					rtword[i] = Alphabet[e+Jump];
+				}
+				else {
+					rtword[i] = Alphabet[e-Jump];
+				}
 				break;
 			}
-			else{
+			else {
 				rtword[i] = word[i];
 			}
 		}
@@ -27,32 +36,8 @@ void Encrypte(char* word){
 	printf("Encrypted-Word: %s\n",rtword);
 	free(rtword);
 }
-void Decrypte(char* word){
- 	const char* Alphabet = "xyzabcdefghijklmnopqrstuvwxyz";
-	int abc_size = strlen(Alphabet);
-	int word_size = strlen(word);
-	char* rtword = (char *)malloc(word_size * sizeof(char));
-
-	for(int i = 0; i<=word_size-1; i++)
-	{
-		for(int e = 0; e<=abc_size; e++)
-		{
-			if(word[i] == Alphabet[e]) {
-				rtword[i] = Alphabet[e-2];
-				break;
-			}
-			else{
-				rtword[i] = word[i];			
-			}
-		}
-	}
-    printf("before: %s\n",word);
-	printf("Decrypted-Word: %s\n",rtword);
-	free(rtword);
-}
 
 int main(void)
 {
-	Encrypte("i am a phrase, and i will be encrypted using the famous caesar cipher! testing some letters here znxmaqirjfhka");
-	Decrypte("vjcvu nkhg oa dtqvjgtttttt");
+	ED("knn dg gpetarvgf", 1);
 }
